268022f09eda7275e8ef7b002313a51aac657f0e
MaryAnne45 <mary.jereh@gmail.com>
2018-09-25 12:24:45 +0300

    displaying images from the nasa api

--- Nasa/views.py
+++ Nasa/views.py
@@ -6,6 +6,9 @@ def landing (request):
     title='Nasa-Api'
     response=requests.get('https://api.nasa.gov/planetary/apod?api_key=EPZMQv6VmlTFXhcGrZ4Jhu1BLZkIjbdjjqVRR2ck')
     data=response.json()
+    title1=data.get('title')
     one=data.get('date')
+    images=data.get('url')
+    hdu=data.get('hdurl')
 
-    return render (request,'Nasa/landing.html',{"title":title,"one":one})
+    return render (request,'Nasa/landing.html',{"title1":title1,"one":one,"images":images,"hdu":hdu})
--- Project/settings.py
+++ Project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'Nasa.apps.NasaConfig',
     'bootstrap3',
     'aiohttp',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
