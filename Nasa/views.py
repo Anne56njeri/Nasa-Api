149df571af52d0a2249c13b59fc4f510b97fdb82
MaryAnne45 <mary.jereh@gmail.com>
2018-09-25 14:35:14 +0300

    added a function to display the camera details

--- Nasa/views.py
+++ Nasa/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .request import all_images
+from .request import all_images,all_camera
 from .models import Sources
 # Create your views here.
 
 def landing (request):
     title='Nasa-Api'
     all_rovers=all_images()
-    
-    return render (request,'Nasa/landing.html',{"all_rovers":all_rovers})
+    camera_details=all_camera()
+    return render (request,'Nasa/landing.html',{"all_rovers":all_rovers,"camera_details":camera_details})
